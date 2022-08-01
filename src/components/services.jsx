12ea23d3e71e19e94ba6d1acef949113bd3f3e7b
webmaster1116 <markveronich@gmail.com>
2022-08-01 15:40:03 +0900

    update for deploy

--- src/components/services.jsx
+++ src/components/services.jsx
@@ -8,12 +8,21 @@ export const Services = (props) => {
         <div className="row">
           <div className="youtube">
             <iframe
+              width="1904"
+              height="760"
+              src="https://www.youtube.com/embed/0n9IsC8jwjI"
+              title="UnrealKingdom Cinematic Forest Castle"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+            {/* <iframe
               className="embed-responsive-item"
               src="https://www.youtube.com/embed/zMyuHLkgMmg?playlist=zMyuHLkgMmg&amp;loop=1&amp;mute=1&amp;showinfo=0&amp;controls=0&amp;start=0&amp;autoplay=1"
               dataAutoplay="1"
               frameBorder="0"
               allowFullScreen=""
-            ></iframe>
+            ></iframe> */}
           </div>
           <div className="register-bottom">
             <a
